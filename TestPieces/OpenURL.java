4ffeb42bc17073e5db8c3ac9f6085889704bdd23
Pujit M. <pujitm2009@gmail.com>
2016-05-13 13:59:22 -0400

    Updated with test pieces in TestPieces Folder

---
+++ TestPieces/OpenURL.java
@@ -0,0 +1,17 @@
+import java.awt.Desktop;
+import java.net.URL;
+
+
+public class OpenURL {
+	public static void openWebpage(String urlString) {
+		try {
+			Desktop.getDesktop().browse(new URL(urlString).toURI());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		openWebpage("http://google.com");
+	}
+}
